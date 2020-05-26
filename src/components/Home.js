eca411fbe13f183e6f634921df71912fbee9aacd
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-05-26 14:12:50 -0600

    fix restaurant card render

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'; 
 import MyLists from './lists/MyLists';
+import Footer from './Footer';
 
 class Home extends React.Component{
 
@@ -11,6 +12,7 @@ class Home extends React.Component{
          <div className="ui message"> 
           <div className="ui center aligned header">
          <br/><h3>A meal app designed to work for you. Enable your location to fully enjoy the features. Organize and keep track of your meal plans, browse recipes or restaurants for inspiration.</h3></div></div> : <MyLists /> }
+        <Footer />
         </div>
       )
   }
